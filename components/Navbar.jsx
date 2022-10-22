cb3b4fb11a4c98f5fed783eeee79d39979a9148f
Onur Ata Asar <onurataasar@gmail.com>
2022-10-22 22:20:49 +0300

    responsive navbar added

---
+++ components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const [style, setStyle] = useState("transparent");
+  const [textStyle, setTextStyle] = useState("white");
+
+  const handleOpenMenu = () => {
+    setMenu(!menu);
+  };
+
+  useEffect(() => {
+    const handleStyle = () => {
+      if (window.scrollY >= 90) {
+        setStyle("white");
+        setTextStyle("black");
+      } else {
+        setStyle("transparent");
+        setTextStyle("white");
+      }
+    };
+    window.addEventListener("scroll", handleStyle);
+  }, []);
+
+  return (
+    <div
+      style={{ backgroundColor: `${style}` }}
+      className="fixed top-0 left-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1 style={{ color: `${textStyle}` }} className="font-bold text-4xl">
+            O.A.Asar
+          </h1>
+        </Link>
+
+        <ul className="hidden sm:flex" style={{ color: `${textStyle}` }}>
+          <li className="p-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/#projects">Projects</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/#skills">Skills</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/#links">Links</Link>
+          </li>
+        </ul>
+
+        {/* Responsive Menu */}
+        <div
+          onClick={handleOpenMenu}
+          className="block sm:hidden z-10 cursor-pointer"
+        >
+          {menu ? (
+            <AiOutlineClose size={30} style={{ color: `${textStyle}` }} />
+          ) : (
+            <AiOutlineMenu size={30} style={{ color: `${textStyle}` }} />
+          )}
+        </div>
+        <div
+          className={
+            menu
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center h-screen w-full ease-in duration-300 bg-black"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 text-center flex justify-center items-center h-screen w-full ease-in duration-300 bg-black"
+          }
+        >
+          <ul>
+            <li className="p-4 text-4xl hover:text-gray-400">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-400">
+              <Link href="/#projects">Projects</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-400">
+              <Link href="/#skills">Skills</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-400">
+              <Link href="/#links">Links</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
