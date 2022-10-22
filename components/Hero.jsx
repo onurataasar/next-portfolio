7b908db2bd74695eed51f42efb64a7b2a6727eca
Onur Ata Asar <onurataasar@gmail.com>
2022-10-22 15:39:50 +0300

    Hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = ({ heading, message }) => {
+  return (
+    <div className="custom-img flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]">
+        <h2 className="text-6xl font-bold">{heading}</h2>
+        <p className="text-2xl py-5">{message}</p>
+        <button className="text-xl px-8 py-2 border">More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
