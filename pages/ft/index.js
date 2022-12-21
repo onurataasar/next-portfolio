1006171d41e46da150f92fdb09cde04b73fb04e7
Onur Ata Asar <onurataasar@gmail.com>
2022-12-22 00:16:59 +0300

    mobile 2

--- pages/ft/index.js
+++ pages/ft/index.js
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 const ft = () => {
   return (
     <div className="bg-black h-[100vh] text-white items-center justify-center flex flex-col gap-12">
-      <h1 className="sm:text-4xl text-2xl">
+      <h1 className="sm:text-4xl text-xl">
         Antalya Dünyanın En Güzel Şehridir{" "}
       </h1>{" "}
       <div className={styles.heart}></div>
