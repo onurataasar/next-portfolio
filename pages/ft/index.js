f7b0262be816e9c7b9228640c7ba50b3fb8e8261
Onur Ata Asar <onurataasar@gmail.com>
2022-12-22 00:12:35 +0300

    ant

---
+++ pages/ft/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+const ft = () => {
+  return (
+    <div className="bg-black h-[100vh] text-white items-center justify-center flex flex-col gap-12">
+      <h1 className="text-4xl">Antalya Dünyanın En Güzel Şehridir </h1>{" "}
+      <div className={styles.heart}></div>
+    </div>
+  );
+};
+
+export default ft;
